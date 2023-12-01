2a0c9cacf2891d693ab82e5a0ee2734f3e40763f
Cerlo <remy.cerlo@viacesi.fr>
2023-12-01 09:22:24 +0100

    🐛 * fix(YachtSquadTokenHolder.sol): fix import statement for IERC1155Receiver ♻️ * chore(YachtSquadTokenHolder.sol): clean up code formatting ✨* feat(YachtSquadTokenHolder.sol): add data parameter to TokenReceived event 🎨 * feat(YachtSquadTokenHolder.sol): update onERC1155Received and onERC1155BatchReceived functions to use new parameter names and store received tokens

--- backend/contracts/YachtSquadTokenHolder.sol
+++ backend/contracts/YachtSquadTokenHolder.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
 contract YachtSquadTokenHolder is ERC1155Holder {
 
@@ -17,7 +16,7 @@ contract YachtSquadTokenHolder is ERC1155Holder {
     mapping(uint256 => TokenInfo) public receivedTokens;
 
     // Event pour signaler la réception d'un token
-    event TokenReceived(address operator, address from, uint256 id, uint256 value);
+    event TokenReceived(address operator, address from, uint256 id, uint256 value, bytes data);
 
     /*
     -
@@ -26,28 +25,28 @@ contract YachtSquadTokenHolder is ERC1155Holder {
      */
     // Override de onERC1155Received pour gérer les tokens reçus
     function onERC1155Received(
-        address operator,
-        address from,
-        uint256 id,
-        uint256 value,
-        bytes calldata data
+        address _operator,
+        address _from,
+        uint256 _id,
+        uint256 _value,
+        bytes memory _data
     ) public virtual override returns (bytes4) {
-        receivedTokens[id] = TokenInfo(value, from);
-        emit TokenReceived(operator, from, id, value);
+        receivedTokens[_id] = TokenInfo(_value, _from);
+        emit TokenReceived(_operator, _from, _id, _value, _data);
         return this.onERC1155Received.selector;
     }
 
     // Override de onERC1155BatchReceived pour gérer les tokens reçus en lot
     function onERC1155BatchReceived(
-        address operator,
-        address from,
-        uint256[] calldata ids,
-        uint256[] calldata values,
-        bytes calldata data
+        address _operator,
+        address _from,
+        uint256[] memory _ids,
+        uint256[] memory _values,
+        bytes memory _data
     ) public virtual override returns (bytes4) {
-        for (uint256 i = 0; i < ids.length; ++i) {
-            receivedTokens[ids[i]] = TokenInfo(values[i], from);
-            emit TokenReceived(operator, from, ids[i], values[i]);
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            receivedTokens[_ids[i]] = TokenInfo(_values[i], _from);
+            emit TokenReceived(_operator, _from, _ids[i], _values[i], _data);
         }
         return this.onERC1155BatchReceived.selector;
     }
