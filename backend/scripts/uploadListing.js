43a542d329e07f01ae6eae74d5905385b60dc62f
Cerlo <remy.cerlo@viacesi.fr>
2023-12-04 22:15:26 +0100

    🔨 ✨ feat(uploadListing.js): refactor code to support bulk upload of assets and corresponding JSON files to Pinata

--- backend/scripts/uploadListing.js
+++ backend/scripts/uploadListing.js
@@ -1,38 +1,73 @@
+/**
+ * Import modules
+ */
 require('dotenv').config();
-const key = process.env.PINATA_KEY;
-const secret = process.env.PINATA_SECRET;
-const pinataSDK = require('@pinata/sdk');
-const pinata = new pinataSDK(key, secret);
+
 const fs = require('fs');
-const readableStreamForFile = fs.createReadStream("mvp/yachtSquad-listing0.png");
+const path = require('path');
+const pinataSDK = require('@pinata/sdk');
 
-const options = {
-    pinataMetadata: {
-        name: "Genesisyach-test",
-    },
-    pinataOptions: {
-        cidVersion: 1
+// Initialize Pinata SDK with the API credentials
+const pinata = new pinataSDK(process.env.PINATA_KEY, process.env.PINATA_SECRET);
+
+// Define the path to the assets folder
+const assetsFolderPath = path.join(__dirname, '../assets');
+
+// Upload fils to Pinata and return IPFS hash
+async function uploadFile(filePath, options) {
+    const readableStreamForFile = fs.createReadStream(filePath);
+    return pinata.pinFileToIPFS(readableStreamForFile, options);
+}
+
+// JSON upload to Pinata and return IPFS hash
+async function uploadJSON(body, options) {
+    return pinata.pinJSONToIPFS(body, options);
+}
+
+async function uploadAssets() {
+    try {
+        // Read all files in assets folder
+        const files = fs.readdirSync(assetsFolderPath);
+
+        // Iterate over each file in the folder
+        for (const file of files) {
+            if (file.endsWith('.png')) {
+                // Set options for image upload
+                const imageOptions = {
+                    pinataMetadata: { name: path.basename(file, '.png') },
+                    pinataOptions: { cidVersion: 1 }
+                };
+
+                // Upload the image file to Pinata
+                const imageResult = await uploadFile(path.join(assetsFolderPath, file), imageOptions);
+                console.log(`Image uploaded: ${imageResult.IpfsHash}`);
+
+                // Construct the corresponding JSON filename
+                const jsonFile = file.replace('.png', '.json');
+                const jsonPath = path.join(assetsFolderPath, jsonFile);
+
+                // JSON?
+                if (fs.existsSync(jsonPath)) {
+                    // Read and parse the JSON file
+                    const jsonData = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
+                    // Update JSON data with IPFS hash
+                    jsonData.image = imageResult.IpfsHash;
+
+                    // Set options for JSON upload
+                    const jsonOptions = {
+                        pinataMetadata: { name: path.basename(jsonFile, '.json') },
+                        pinataOptions: { cidVersion: 1 }
+                    };
+
+                    // Upload JSON file to Pinata
+                    const jsonResult = await uploadJSON(jsonData, jsonOptions);
+                    console.log(`JSON uploaded: ${jsonResult.IpfsHash}`);
+                }
+            }
+        }
+    } catch (error) {
+        console.error('Error uploading assets:', error);
     }
-};
+}
 
-
-pinata.pinFileToIPFS(readableStreamForFile, options).then((result) => {
-    const body = {
-        name : options.pinataMetadata.name,//YachtName
-        image : result.IpfsHash,//CID image
-        mmsi : "227999700", //mmsi/AIS
-        imo : "",//pas pour la plaisance
-        legal : "",//hashcontract
-        sn : "",//SerialNumber
-        paymentWallet : ""//Dividende payment wallet
-    };
-
-    pinata.pinJSONToIPFS(body, options).then((json) => {
-        console.log(json);
-    }).catch((err) => {
-        console.log(err);
-    });
-
-}).catch((err) => {
-    console.log(err);
-});
+uploadAssets();
