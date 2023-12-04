7d3f9e9bcaaf2d617d886dd7d57a07e18011bcb6
Cerlo <remy.cerlo@viacesi.fr>
2023-12-04 16:07:03 +0100

    ✅ - feat(YachtSquadTokenHolder.test.js): add multiple tokens handling test ✅ - feat(YachtSquadTokenHolder.test.js): add token batch transfer test ✅ - feat(YachtSquadTokenHolder.test.js): add get token info test ♻️ -refactor(YachtSquadTokenHolder.test.js): remove unnecessary comment

--- backend/test/YachtSquadTokenHolder.test.js
+++ backend/test/YachtSquadTokenHolder.test.js
@@ -25,6 +25,18 @@ describe("YachtSquadTokenHolder contract", function () {
         status : status.IntialMint
     }
 
+    const yacht1= {
+        id:1,
+        mmsi:319085900, //mmsi/AIS => yacht identification
+        tokenPrice:1000, 
+        maxSupply:100000,
+        name: 'Aquijo',
+        uri:'cb8480dedb3a9f7fbb1d5707e228d80c119fc57184651bdecfdb1cef9c0dc899',
+        legal: 'a899fc0d56ef54e9a2a9e7b8ef16e79f4ca11a56d86d79f6366d14b0c3c690aa', //Aquijo is owned by YachtSquad
+        paymentWallet:"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        status : status.PreSale
+    }
+
     async function deployContracts() {
         const [owner, investor, yachtCharterCompany] = await ethers.getSigners();
 
@@ -55,8 +67,59 @@ describe("YachtSquadTokenHolder contract", function () {
             expect(tokenInfo.amount).to.equal(yacht0.maxSupply);
             expect(tokenInfo.sender).to.equal(constants.ZERO_ADDRESS); // The initial mint must be from ZERO_ADDRESS
         });
+        it("Should handle receiving multiple tokens correctly", async function () {
+            const { yachtSquadTokenHolder, yachtSquadTokenization, owner, investor, yachtCharterCompany } = await loadFixture(deployContracts);
+            // Mint two yachts to the token holder
+            const tokenIds = [0,1];
+            const amounts = [10000,5000];
+            // Mint each yacht to the token holder
+            await yachtSquadTokenization.connect(owner).mintyachts(
+                yachtSquadTokenHolder.target, 
+                yacht0.mmsi, 
+                yacht0.tokenPrice,
+                yacht0.maxSupply, // Use the corresponding amount for each yacht
+                yacht0.name, 
+                yacht0.uri,  
+                yacht0.legal, 
+                yachtCharterCompany.address
+            );
+            await yachtSquadTokenization.connect(owner).mintyachts(
+                yachtSquadTokenHolder.target, 
+                yacht1.mmsi, 
+                yacht1.tokenPrice,
+                yacht1.maxSupply, // Use the corresponding amount for each yacht
+                yacht1.name, 
+                yacht1.uri,  
+                yacht1.legal, 
+                yachtCharterCompany.address
+            );
+                // Set the YachtSquadTokenization contract as the authorized caller in YachtSquadTokenHolder
+            await yachtSquadTokenHolder.connect(owner).setYachtSquadTokenisationContract(investor.address);
+
+            // Transfer tokens from the holder to an investor
+            await yachtSquadTokenHolder.connect(investor).transferTokenBatch( investor.address, tokenIds, amounts);
+
+
+            // Simulate batch transfer (this part may vary depending on how your contracts are set up)
+            // Assuming yachtSquadTokenization contract has a method to batch transfer tokens
+            await yachtSquadTokenization.connect(investor).safeBatchTransferFrom(
+                investor.address, 
+                yachtSquadTokenHolder.target, 
+                tokenIds, 
+                amounts, 
+                "0x"
+            );
+    
+            // Verify that each token is correctly recorded in the receivedTokens mapping
+            for (let i = 0; i < tokenIds.length; i++) {
+                const tokenInfo = await yachtSquadTokenHolder.getTokenInfo(tokenIds[i]);
+                expect(tokenInfo.amount).to.equal(amounts[i]);
+                expect(tokenInfo.sender).to.equal(investor.address); // Assuming the owner is the one who transferred the tokens
+            }
+        });
     }); // token reception
 
+    //add security tests
     describe("Token Transfer", function () {
         it("Should transfer a token correctly", async function () {
             const { yachtSquadTokenHolder, yachtSquadTokenization, owner, investor,yachtCharterCompany } = await loadFixture(deployContracts);
@@ -75,7 +138,6 @@ describe("YachtSquadTokenHolder contract", function () {
                 yachtCharterCompany.address
             );
     
-            //0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 (owner)   ----- 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512 (yachtSquadTokenHolder)
             await yachtSquadTokenHolder.connect(owner).setYachtSquadTokenisationContract(investor.address);
             await yachtSquadTokenHolder.connect(investor).transferToken(investor.address, tokenId, amount);
     
@@ -87,5 +149,97 @@ describe("YachtSquadTokenHolder contract", function () {
             expect(balanceInvestor).to.equal(amount);
         });
     }); // end token transfert
+
+    //add security tests
+    describe("Token Batch Transfer", function () {
+        it("Should transfer multiple tokens correctly", async function () {
+            const { yachtSquadTokenHolder, yachtSquadTokenization, owner, investor, yachtCharterCompany } = await loadFixture(deployContracts);
+
+            // Mint two yachts to the token holder
+            const tokenId0 = yacht0.id;
+            const tokenId1 = yacht1.id;
+            const amount0 = 10000;
+            const amount1 = 5000;
+
+            await yachtSquadTokenization.connect(owner).mintyachts(
+                yachtSquadTokenHolder.target, 
+                yacht0.mmsi, 
+                yacht0.tokenPrice,
+                yacht0.maxSupply,
+                yacht0.name, 
+                yacht0.uri,  
+                yacht0.legal, 
+                yachtCharterCompany.address
+            );
+
+            await yachtSquadTokenization.connect(owner).mintyachts(
+                yachtSquadTokenHolder.target, 
+                yacht1.mmsi, 
+                yacht1.tokenPrice,
+                yacht1.maxSupply,
+                yacht1.name, 
+                yacht1.uri,  
+                yacht1.legal, 
+                yachtCharterCompany.address
+            );
+
+            // Set the YachtSquadTokenization contract as the authorized caller in YachtSquadTokenHolder
+            await yachtSquadTokenHolder.connect(owner).setYachtSquadTokenisationContract(investor.address);
+
+            // Transfer tokens from the holder to an investor
+            await yachtSquadTokenHolder.connect(investor).transferTokenBatch( investor.address, [tokenId0, tokenId1], [amount0, amount1]);
+
+            // Check balances for both tokens
+            const balanceHolder0 = await yachtSquadTokenization.balanceOf(yachtSquadTokenHolder.target, tokenId0);
+            const balanceInvestor0 = await yachtSquadTokenization.balanceOf(investor.address, tokenId0);
+            const balanceHolder1 = await yachtSquadTokenization.balanceOf(yachtSquadTokenHolder.target, tokenId1);
+            const balanceInvestor1 = await yachtSquadTokenization.balanceOf(investor.address, tokenId1);
+
+            expect(balanceHolder0).to.equal(yacht0.maxSupply - amount0);
+            expect(balanceInvestor0).to.equal(amount0);
+            expect(balanceHolder1).to.equal(yacht1.maxSupply - amount1);
+            expect(balanceInvestor1).to.equal(amount1);
+        });
+    }); // end token Batch transfert
+
+    describe("Get Token Info", function () {
+        it("Should return correct information for a received token", async function () {
+            const { yachtSquadTokenHolder, yachtSquadTokenization, owner, yachtCharterCompany } = await loadFixture(deployContracts);
+    
+            // Mint a yacht to the token holder
+            const tokenId = yacht0.id;
+            await yachtSquadTokenization.connect(owner).mintyachts(
+                yachtSquadTokenHolder.target, 
+                yacht0.mmsi, 
+                yacht0.tokenPrice,
+                yacht0.maxSupply,
+                yacht0.name, 
+                yacht0.uri,  
+                yacht0.legal, 
+                yachtCharterCompany.address
+            );
+    
+            // Retrieve token information
+            const tokenInfo = await yachtSquadTokenHolder.getTokenInfo(tokenId);
+    
+            // Check if the token information is correct
+            expect(tokenInfo.amount).to.equal(yacht0.maxSupply);
+            expect(tokenInfo.sender).to.equal(constants.ZERO_ADDRESS); // Assuming the owner is the one who minted the token
+        });
+    
+        it("Should return zero information for a non-received token", async function () {
+            const { yachtSquadTokenHolder } = await loadFixture(deployContracts);
+    
+            // Use a tokenId that has not been minted or transferred
+            const nonExistentTokenId = 999; // Example token ID that hasn't been used
+    
+            // Retrieve token information
+            const tokenInfo = await yachtSquadTokenHolder.getTokenInfo(nonExistentTokenId);
+    
+            // Check if the token information shows zero values
+            expect(tokenInfo.amount).to.equal(0);
+            expect(tokenInfo.sender).to.equal(constants.ZERO_ADDRESS); // ZERO_ADDRESS for a non-received token
+        });
+    }); //Get Token Info
 
 });
