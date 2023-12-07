3b18e90ea89743ba5d0be20cd6bd0c2c98775932
Cerlo <remy.cerlo@viacesi.fr>
2023-12-07 03:49:24 +0100

    ✨ - feat(Card.js): add price formatting and total price calculation ✨ - feat(Card.js): add status text based on status code 💄 - style(Card.js): adjust layout for total price and buy button

--- frontend/src/app/components/Card/Card.js
+++ frontend/src/app/components/Card/Card.js
@@ -2,56 +2,76 @@
 import React, { useState } from 'react';
 
 const Card = ({ data }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);    const statusText = ["Initial Mint", "PreSale", "Public Sale", "Chartered", "Maintenance", "Sold"];
   // Remplacer le préfixe IPFS par un lien HTTP
   const ipfsImageUrl = data.uri.replace('ipfs://', 'https://ipfs.io/ipfs/');
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
 
+  // Format price 100 000 instead of 100000
+  const formatPrice = (price) => {
+    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
+  // Calcul du prix total
+  const totalPrice = BigInt(data.tokenPrice) * BigInt(data.maxSupply);
+  const formattedTotalPrice = formatPrice(totalPrice);
+
+  const getStatusText = (status) => {
+    const statusText = ["Initial Mint", "PreSale", "Public Sale", "Chartered", "Maintenance", "Sold"];
+    return statusText[status] || "Unknown";
+  };
+
+
   return (
     <div className="flex justify-center items-center my-4">
-    <div className="card card-bordered bg-lessDark w-1/2 max-w-600px flex flex-row rounded-none h-auto md:h-96">
-      {/* Image à gauche */}
-      <figure className="flex-1 cursor-pointer  transition-transform ">
-        <img
-          className="object-cover w-full h-full hover:scale-105 overflow-hidden duration-300"
-          src={ipfsImageUrl}
-          alt={data.name}
-        />
-      </figure>
+      <div className="card card-bordered bg-lessDark w-1/2 max-w-600px flex flex-row rounded-none h-auto md:h-96">
+        {/* Image à gauche */}
+        <figure className="flex-1 cursor-pointer transition-transform">
+          <img
+            className="object-cover w-full h-full hover:scale-105 overflow-hidden duration-300"
+            src={ipfsImageUrl}
+            alt={data.name}
+          />
+        </figure>
 
-      {/* Contenu de la carte à droite */}
-      <div className="card-body flex-1 flex flex-col justify-between">
-        <div>
-          <h2 className="card-title text-gold">{data.name}</h2>
-          <p>
-            MMSI: {data.mmsi.toString()}<br />
-            Prix du Token: {data.tokenPrice.toString()}<br />
-            Max Supply: {data.maxSupply.toString()}<br />
-            Wallet de Paiement: {data.paymentWallet}<br />
-            Status: {data.status}
-          </p>
-        </div>
-        <div className="card-actions justify-center">
-          <button className="rounded-none btn btn-primary border-dark bg-gold hover:bg-lessDark hover:text-gold hover:border-gold" onClick={openModal}>
-            Buy {data.name}
-          </button>
-        </div>
-      </div>
-
-      {/* Modal */}
-      {isOpen && (
-        <div className="modal modal-open">
-          <div className="modal-box">
-            <img src={ipfsImageUrl} alt={data.name} />
-            <div className="modal-action">
-              <button onClick={closeModal} className="btn">Close</button>
+        {/* Contenu de la carte à droite */}
+        <div className="card-body flex-1 flex flex-col justify-between">
+          <div>
+            <h2 className="card-title text-gold">{data.name}</h2>
+            <p>
+              MMSI: {data.mmsi.toString()}<br />
+              Prix du Token: {data.tokenPrice.toString()}<br />
+              Max Supply: {data.maxSupply.toString()}<br />
+              Wallet de Paiement: {data.paymentWallet}<br />
+              Status: {getStatusText(data.status)}
+            </p>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-gold text-lg font-semibold">
+              Prix Total: {formattedTotalPrice} $
+            </p>
+            <div className="card-actions">
+              <button className="rounded-none btn btn-primary border-dark bg-gold hover:bg-lessDark hover:text-gold hover:border-gold" onClick={openModal}>
+                Buy {data.name}
+              </button>
             </div>
           </div>
         </div>
-      )}
+
+        {/* Modal */}
+        {isOpen && (
+          <div className="modal modal-open">
+            <div className="modal-box">
+              <img src={ipfsImageUrl} alt={data.name} />
+              <div className="modal-action">
+                <button onClick={closeModal} className="btn">Close</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
   );
 };
 
